7691dfef36b0e35d1999ae0218d01503b067de35
yxc666 <410014739@qq.com>
2017-11-05 19:26:46 +0800

    Site updated: 2017-11-05 19:26:45

--- js/src/algolia-search.js
+++ js/src/algolia-search.js
@@ -39,7 +39,7 @@ $(document).ready(function () {
         item: function (data) {
           var link = data.permalink ? data.permalink : (CONFIG.root + data.path);
           return (
-            '<a target="_blank" href="' + link + '" class="algolia-hit-item-link">' +
+            '<a href="' + link + '" class="algolia-hit-item-link">' +
               data._highlightResult.title.value +
             '</a>'
           );
