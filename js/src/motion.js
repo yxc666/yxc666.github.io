695e3b521aac61a6ca960a5cbe532d8dc0033eae
yxc666 <410014739@qq.com>
2017-11-24 10:58:29 +0800

    Site updated: 2017-11-24 10:58:10

--- js/src/motion.js
+++ js/src/motion.js
@@ -342,6 +342,6 @@ $(document).ready(function () {
   $('.tag-cloud a').each(function(){
     var fsize = 12 + Math.random()*5;
     this.style.fontSize = fsize+'px';
-    consolle.log(this)
+    console.log(this)
   })
 });
