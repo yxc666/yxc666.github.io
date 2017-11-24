3c9000f998065b84b0243fd243b02edab3773cc8
yxc666 <410014739@qq.com>
2017-11-24 11:31:20 +0800

    Site updated: 2017-11-24 11:31:02

--- js/src/motion.js
+++ js/src/motion.js
@@ -340,10 +340,11 @@ $(document).ready(function () {
   };
 
   $('.tag-cloud a').each(function(){
-    var rn =Math.floor(30 + Math.random()*80);
+    var rn =Math.floor(Math.random()*180);
     var rdcolor = `rgb(${rn},${rn},${rn})`;
-    var rdsize =Math.floor(12 + Math.random()*20);
+    var rdsize =Math.floor(12 + Math.random()*25);
     this.style.fontSize = rdsize+'px';
     this.style.color = rdcolor;
+    this.style.borderBottomColor = rdcolor;
   })
 });
