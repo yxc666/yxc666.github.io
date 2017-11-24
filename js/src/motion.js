fab903519d8cf22f3284471f7f9a592f8fd64a8d
yxc666 <410014739@qq.com>
2017-11-24 16:09:13 +0800

    Site updated: 2017-11-24 16:08:58

--- js/src/motion.js
+++ js/src/motion.js
@@ -339,12 +339,12 @@ $(document).ready(function () {
     }
   };
 
-  $('.tag-cloud a').each(function(){
-    var rn =Math.floor(Math.random()*180);
-    var rdcolor = `rgb(${rn},${rn},${rn})`;
-    var rdsize =Math.floor(12 + Math.random()*20);
-    this.style.fontSize = rdsize+'px';
-    this.style.color = rdcolor;
-    this.style.borderBottomColor = rdcolor;
-  })
+  // $('.tag-cloud a').each(function(){
+  //   var rn =Math.floor(Math.random()*180);
+  //   var rdcolor = `rgb(${rn},${rn},${rn})`;
+  //   var rdsize =Math.floor(12 + Math.random()*20);
+  //   this.style.fontSize = rdsize+'px';
+  //   this.style.color = rdcolor;
+  //   this.style.borderBottomColor = rdcolor;
+  // })
 });
