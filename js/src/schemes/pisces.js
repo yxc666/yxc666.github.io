818308ee6861dce851eec2a5e032d58de69869b0
yxc666 <410014739@qq.com>
2017-11-25 09:30:52 +0800

    Site updated: 2017-11-25 09:30:36

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -14,4 +14,16 @@ $(document).ready(function () {
       setSidebarMarginTop(getSidebarTop());
     }
   });
+
+  $(document).ready(function(){
+    $('.tag-cloud-tags a').each(function(){
+      var rn =Math.floor(Math.random()*180);
+      var rdcolor = `rgb(${rn},${rn},${rn})`;
+      var rdsize =Math.floor(12 + Math.random()*20);
+      this.style.fontSize = rdsize+'px';
+      this.style.color = rdcolor;
+      this.style.borderBottomColor = rdcolor;
+    })
+  })
+
 });
